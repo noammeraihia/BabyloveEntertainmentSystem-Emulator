d5bee377b8808bf2b2131a7aa517229fb6ace7b8
Noam MERAIHIA <meraihianoam@gmail.com>
2024-07-27 01:42:04 +0200

    cpu instructions tables added +  fixed some bugs

--- src/bes_main.c
+++ src/bes_main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "CPU/bes_6502cpu.h"
+
 int main(int argc, char* argv[])
 {
     printf("Hello BES !\n");
+
+    bes6502CPU_t CPU = bes6502CPU_create();
+    besBus_t bus = besBus_create(&CPU);
+
+    bes6502CPU_write(&CPU, 0xf0f0, 0xF9);
+    printf("%x\n", bes6502CPU_read(&CPU, 0xf0f0));
 
     return 0;
 }
